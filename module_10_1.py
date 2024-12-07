a63603ea32ea99f23369d84b8af0355b53f6b327
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-12-07 18:32:12 +0400

    Add files via upload

--- module_10_1.py
+++ module_10_1.py
@@ -9,37 +9,38 @@
 # Взятие текущего времени
 # Вывод разницы начала и конца работы потоков
 
-import threading
-import time
+from threading import Thread
+from time import sleep
 from datetime import datetime
 
 
 def write_words(word_count, file_name):
-    word_count = 0
     with open(file_name, 'w', encoding='utf-8') as file:
-        while word_count < 10:
-            time.sleep(0.1)
-            word_count += 1
-            file.write(f'Какое-то слово №{word_count}\n')
-
+        for i in range(word_count):
+            i += 1
+            file.write(f'Какое-то слово №{i}\n')
+            sleep(0.1)
     print(f'Завершилась запись в файл {file.name}')
 
 
 time_start = datetime.now()
+
 write_words(10, 'example1.txt')
 write_words(30, 'example2.txt')
 write_words(200, 'example3.txt')
 write_words(100, 'example4.txt')
+
 time_stop = datetime.now()
-res_time = time_stop - time_start
-print(f'Работа функции {res_time}')
+time_res = time_stop - time_start
+print(f'Работа функции {time_res}')
 
-thread1 = threading.Thread(write_words(10, 'example5.txt'))
-thread2 = threading.Thread(write_words(30, 'example6.txt'))
-thread3 = threading.Thread(write_words(200, 'example7.txt'))
-thread4 = threading.Thread(write_words(100, 'example8.txt'))
+time_start2 = datetime.now()
 
-time2_start = datetime.now()
+thread1 = Thread(write_words(10, 'example5.txt'))
+thread2 = Thread(write_words(30, 'example6.txt'))
+thread3 = Thread(write_words(200, 'example7.txt'))
+thread4 = Thread(write_words(100, 'example8.txt'))
+
 thread1.start()
 thread2.start()
 thread3.start()
@@ -50,6 +51,6 @@ thread2.join()
 thread3.join()
 thread4.join()
 
-time2_stop = datetime.now()
-res2_time = time2_stop - time2_start
-print(f'Работа потоков {res2_time}')
+time_stop2 = datetime.now()
+time_res2 = time_stop2 - time_start2
+print(f'Работа потоков {time_res2}')
