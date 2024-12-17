c5deb70716955af8dfb103642c0754dda739451e
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-12-17 21:23:23 +0400

    Add files via upload

--- module_10_4.py
+++ module_10_4.py
@@ -38,17 +38,16 @@ class Cafe:
                 print(f'{guest.name} в очереди')
 
     def discuss_guests(self):
-        while True:
+        while not self.queue.empty() or any(table.guest is not None for table in self.tables):
             for table in self.tables:
-                if (not table.guest is None) and (table.guest.is_alive()):
+                if not table.guest is None and not table.guest.is_alive():
                     print(f'{table.guest.name} покушал(-а) и ушёл(ушла)')
                     print(f'Стол номер {table.number} свободен')
                     table.guest = None
-                if (not self.queue.empty()) and (table.guest is None):
+                if not self.queue.empty():
                     table.guest = self.queue.get()
                     print(f'{table.guest.name} вышел(-ла) из очереди и сел(-а) за стол номер {table.number}')
                     table.guest.start()
-
 
 
 # Создание столов
